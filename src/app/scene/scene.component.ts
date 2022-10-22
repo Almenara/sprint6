3e292e1f0aac0f85a2ca102447915fbafe26e9b5
albertoalmenara <PeopleRK>
2022-10-22 17:25:10 +0200

    Sprint 6 / Ejercicio 6

--- src/app/scene/scene.component.ts
+++ src/app/scene/scene.component.ts
@@ -1,3 +1,4 @@
+import { Sentence } from './../home/sentence.model';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,12 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./scene.component.scss']
 })
 export class SceneComponent implements OnInit {
-  @Input() story:string[];
+  @Input() story:Sentence[];
 
   public currentSentence:number = 0;
+  public currentImage:String = "";
   public enterScene:boolean = false;
+  public bodyHtml = document.getElementsByTagName('body')[0];
 
   constructor() {
   }
@@ -18,12 +21,19 @@ export class SceneComponent implements OnInit {
   }
 
   next(){
-    if(this.currentSentence < (this.story.length - 1)) this.currentSentence++
+    if(this.currentSentence < (this.story.length - 1)){
+      this.currentSentence++;
+      this.bodyHtml.style.backgroundImage = `url('${this.story[this.currentSentence].backgroundImg}')`;
+    }
   }
   prev(){
-    if(this.currentSentence > 0) this.currentSentence--
+    if(this.currentSentence > 0){
+      this.currentSentence--;
+      this.bodyHtml.style.backgroundImage = `url('${this.story[this.currentSentence].backgroundImg}')`;
+    }
   }
   showStory(){
+    this.bodyHtml.style.backgroundImage = `url('${this.story[this.currentSentence].backgroundImg}')`;
     this.enterScene=true;
   }
 }
